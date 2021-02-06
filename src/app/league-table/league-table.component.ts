b2797f03d3dc055f77ed763764ebfe7c4535d53e
Osanda <osanda.v13@gmail.com>
2021-02-06 17:49:11 +0530

    orderByPoints function added

--- src/app/league-table/league-table.component.ts
+++ src/app/league-table/league-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ILeagueTeams } from "../league-teams.model";
+import {PremierLeagueService} from "../services/premier-league.service";
 @Component({
   selector: 'app-league-table',
   templateUrl: './league-table.component.html',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeagueTableComponent implements OnInit {
 
-  constructor() { }
+  public leagueTeams: ILeagueTeams[] = [];
+  private pointsDescendingOrder = false;
+  private goalsDescendingOrder = false;
+  private winsDescendingOrder = false;
 
-  ngOnInit(): void {
+  constructor(private service: PremierLeagueService) {
   }
 
+  ngOnInit(): void {
+    this.service.fetchLeagueTeams()
+      .subscribe((teams: any) => {
+        for (const team in teams) {
+          this.leagueTeams.push(teams[team]);
+        }
+      })
+  }
+
+  orderByPoints() {
+    if (!this.pointsDescendingOrder) {
+      this.pointsDescendingOrder = true
+      this.leagueTeams.sort(this.desendingOrderOfPoints)
+    } else {
+      this.pointsDescendingOrder = false
+      this.leagueTeams.sort(this.assendingOrderOfPoints)
+    }
+  }
 }
