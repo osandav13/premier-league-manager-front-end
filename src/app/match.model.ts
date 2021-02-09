bc5e5fb6f2f1ab5ce3ae9fd24bc7901c01858e8f
Osanda <osanda.v13@gmail.com>
2021-02-09 23:36:40 +0530

    match model added

---
+++ src/app/match.model.ts
@@ -0,0 +1,10 @@
+export interface IMatch {
+  firstClubName: string;
+  secondClubName: string;
+  matchName: string;
+  goalsByFirstClub: number;
+  goalsBySecondClub: number;
+  matchDate: string;
+  winner: string;
+  score: string;
+}
--- src/app/services/premier-league.service.ts
+++ src/app/services/premier-league.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {ILeagueTeams} from "../league-teams.model";
-import {Observable} from "rxjs";
-import {IMatch} from "../../match.model";
+import {HttpClient} from '@angular/common/http';
+import {ILeagueTeams} from '../league-teams.model';
+import {Observable} from 'rxjs';
+import {IMatch} from '../match.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PremierLeagueService {
   private BASE_URL= 'http://localhost:8080/api/';
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  fetchMatchHistory():Observable<IMatch[]>{
+  fetchMatchHistory(): Observable<IMatch[]>{
     return this.http.get<IMatch[]>(this.BASE_URL + 'matchHistory')
   }
 
-  fetchLeagueTeams():Observable<ILeagueTeams[]>{
+  fetchLeagueTeams(): Observable<ILeagueTeams[]>{
     return this.http.get<ILeagueTeams[]>(this.BASE_URL + 'leagueTable')
   }
 
