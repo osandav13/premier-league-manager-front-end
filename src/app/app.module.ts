7e67ab1ac2bd6010a1da0490b25600e3ecaa73d2
Osanda <osanda.v13@gmail.com>
2021-02-09 23:39:21 +0530

    match-history component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeagueTableComponent } from './league-table/league-table.component';
 import {HttpClientModule} from '@angular/common/http';
+import { MatchHistoryComponent } from './match-history/match-history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeagueTableComponent
+    LeagueTableComponent,
+    MatchHistoryComponent
   ],
   imports: [
     BrowserModule,
