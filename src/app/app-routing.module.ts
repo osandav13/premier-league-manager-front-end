e7866198fa08468981ea7960dd59d5fde9733617
Osanda <osanda.v13@gmail.com>
2021-02-13 22:26:36 +0530

    routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LeagueTableComponent} from './league-table/league-table.component';
+import {MatchHistoryComponent} from './match-history/match-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LeagueTableComponent},
+  {path: 'leagueTable', component: LeagueTableComponent},
+  {path: 'matchHistory', component: MatchHistoryComponent},
+  {path: '**', component: LeagueTableComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
